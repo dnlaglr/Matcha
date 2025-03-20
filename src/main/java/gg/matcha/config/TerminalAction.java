16f6a8f858e193f98ac7d0ee0ca659528127b73e
DanielAglr <155811921+dnlaglr@users.noreply.github.com>
2025-03-20 03:08:40 -0700

    Create TerminalAction enums

---
+++ src/main/java/gg/matcha/config/TerminalAction.java
@@ -0,0 +1,13 @@
+package gg.matcha.config;
+
+public enum TerminalAction {
+    COPY,
+    PASTE,
+    EXECUTE,
+    DELETE_CHAR,
+    PREVIOUS_COMMAND,
+    NEXT_COMMAND,
+    CLEAR_SCREEN,
+    INTERRUPT_PROCESS,
+    NONE
+}
